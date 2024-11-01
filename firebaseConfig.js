3904ccda57ff823bd0b3d875d7ad2fd3095dc4d9
Harsha Ninganna <121672013+HarshaNinganna@users.noreply.github.com>
2024-11-01 19:02:47 +0530

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAk9QvTDGtFvIACEimaG_7a_bcE8YVJAgA",
+  authDomain: "wattnest.firebaseapp.com",
+  projectId: "wattnest",
+  storageBucket: "wattnest.firebasestorage.app",
+  messagingSenderId: "674947608308",
+  appId: "1:674947608308:web:c043138faf9e56934a78a1",
+  measurementId: "G-RNSLPM1FNH"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
